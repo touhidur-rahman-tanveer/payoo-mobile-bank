800f2d2fc5d1ea20361d4f01dab3b3479dde01cf
touhidur rahman tanveer <touhidurrahmantanveer@gmail.com>
2024-09-23 00:53:37 +0600

    file name changed

--- js files/main.js
+++ js files/main.js
@@ -2,7 +2,7 @@
 // -------- log Out -------
 
 document.getElementById('logout-btn').addEventListener('click', function(){
-    window.location.href = './login.html'
+    window.location.href = './index.html'
 })
 
 
